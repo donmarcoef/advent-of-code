6cd941dd7be2da1e5860ffc267383a211efc0c12
Marco Zimmermann <marco.m.zimmermann@deutschebahn.com>
2021-12-01 12:15:40 +0100

    Day 1 - part two

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,8 +1,8 @@
-fun nrOfIncreases(input: List<String>): Int {
+fun nrOfIncreases(input: List<Int>): Int {
     var previousValue: Int? = null
     var nrOfIncreases = 0
 
-    input.map { it.toInt() }.forEach { current ->
+    input.forEach { current ->
         previousValue?.let {
             if (current > it) {
                 nrOfIncreases++
@@ -15,6 +15,42 @@ fun nrOfIncreases(input: List<String>): Int {
     return nrOfIncreases
 }
 
+fun nrOfIncreasesFromSlices(input: List<List<Int>>) =
+    nrOfIncreases(input.map {
+        it.sum()
+    })
+
+fun <T> slice(input: List<T>, sliceCount: Int): List<List<T>> {
+    var completeList = mutableListOf<List<T>>()
+    var currentSlice = mutableListOf<T>()
+
+    val iterator = input.iterator()
+    while (true) {
+        if (currentSlice.isNotEmpty()) {
+            if (currentSlice.size >= sliceCount) {
+                completeList.add(currentSlice.slice(IntRange(0, sliceCount - 1)))
+
+                currentSlice.removeFirstOrNull()
+            } else {
+                break
+            }
+        }
+
+        for (i in 1..sliceCount) {
+            if (iterator.hasNext()) {
+                currentSlice.add(iterator.next())
+            } else {
+                break
+            }
+        }
+    }
+
+    return completeList
+}
+
 fun main() {
-    println(nrOfIncreases(readInput("Day01")))
+    val input = readInput("Day01").map { it.toInt() }
+    val slicedInput = slice(input, 3)
+
+    println(nrOfIncreasesFromSlices(slicedInput))
 }
--- src/test/kotlin/Day01KtTest.kt
+++ src/test/kotlin/Day01KtTest.kt
@@ -8,7 +8,37 @@ internal class Day01KtTest {
 
     @Test
     fun testIncrease() {
-        Assertions.assertEquals(7, nrOfIncreases(readInput("Day01_test", Day01KtTest::class.java)))
+        Assertions.assertEquals(7, nrOfIncreases(readTestData().map { it.toInt() }))
     }
 
+    @Test
+    fun slice() {
+        val listOfSlices = slice(readTestData(), 3)
+        Assertions.assertEquals(8, listOfSlices.size)
+
+        val expectedList = listOf(
+            listOf("199", "200", "208"),
+            listOf("200", "208", "210"),
+            listOf("208", "210", "200"),
+            listOf("210", "200", "207"),
+            listOf("200", "207", "240"),
+            listOf("207", "240", "269"),
+            listOf("240", "269", "260"),
+            listOf("269", "260", "263")
+        )
+
+        Assertions.assertEquals(expectedList, listOfSlices)
+    }
+
+    @Test
+    fun nrOfIncreasesFromSlices() {
+        val slicedList = slice(readTestData(), 3).map {
+            it.map {
+                it.toInt()
+            }
+        }
+        Assertions.assertEquals(5, nrOfIncreasesFromSlices(slicedList))
+    }
+
+    private fun readTestData() = readInput("Day01_test", Day01KtTest::class.java)
 }
