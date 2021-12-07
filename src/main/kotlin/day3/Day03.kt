e0cb843636139a6601dfbfb6264287ef289ac695
Marco Zimmermann <marco.m.zimmermann@deutschebahn.com>
2021-12-07 08:34:42 +0100

    Day 3 - part two

--- src/main/kotlin/day3/Day03.kt
+++ src/main/kotlin/day3/Day03.kt
@@ -6,11 +6,27 @@ import kotlin.math.pow
 /**
  * @author Team Nexus
  **/
-fun binaryDiagnostic(binayNumbers:List<String>):Int {
+fun binaryDiagnostic(binayNumbers: List<String>): Int {
+    val positionNumberInformation = analyseNumbers(binayNumbers)
+
+    val length = positionNumberInformation.keys.reduce { acc, i -> if (i > acc) i else acc }
+
+    var gamma = ""
+    for (i in IntRange(0, length)) {
+        gamma += positionNumberInformation[i]?.mostCommonString()
+            ?: throw IllegalStateException("no information for position $i")
+    }
+
+    val epsilon = flipBinay(gamma)
+
+    return binaryToNumber(gamma) * binaryToNumber(epsilon)
+}
+
+private fun analyseNumbers(binayNumbers: List<String>): MutableMap<Int, NumberInformation> {
     val positionNumberInformation = mutableMapOf<Int, NumberInformation>()
 
     binayNumbers.forEach {
-        for (i in it.indices)  {
+        for (i in it.indices) {
             val positionInfo = positionNumberInformation[i] ?: NumberInformation()
 
             val char = it[i]
@@ -24,20 +40,52 @@ fun binaryDiagnostic(binayNumbers:List<String>):Int {
             positionNumberInformation[i] = positionInfo
         }
     }
-
-    val length = positionNumberInformation.keys.reduce { acc, i -> if (i > acc) i else acc }
-
-    var gamma = ""
-    for (i in IntRange(0, length)) {
-        gamma += positionNumberInformation[i]?.commonString()?:throw IllegalStateException("no information for position $i")
-    }
-
-    val epsilon = flipBinay(gamma)
-
-    return binaryToNumber(gamma) * binaryToNumber(epsilon)
+    return positionNumberInformation
 }
 
-fun flipBinay(binay:String):String {
+fun lifeSupportingRate(binaryNumbers: List<String>, keep:(numberInformation:MutableMap<Int, NumberInformation>, number:String, pos:Int)->Boolean): Int {
+    val length = binaryNumbers.first().length
+    var pos = 0
+    var toKeep = binaryNumbers.toMutableList()
+
+    while(toKeep.size != 1 && pos < length) {
+        val numberAnalyse = analyseNumbers(toKeep)
+        val newToKeep = mutableListOf<String>()
+
+        toKeep.forEach {
+            if (keep(numberAnalyse, it, pos)) {
+                newToKeep.add(it)
+            }
+        }
+        toKeep = newToKeep
+
+        pos++
+    }
+
+    if (toKeep.size != 1) {
+        throw IllegalStateException("not exactly one number left (${toKeep.size})")
+    }
+
+    return binaryToNumber(toKeep.first())
+}
+
+fun oxygenGeneratorRating() = { numberInformation:MutableMap<Int, NumberInformation>, binaryNumber:String, pos:Int ->
+    val nrOfOne = numberInformation[pos]!!.nrOfOne
+    val nrOfZero = numberInformation[pos]!!.nrOfZero
+    (binaryNumber[pos] == '1' && nrOfOne >= nrOfZero)
+            ||
+            (binaryNumber[pos] == '0' && nrOfZero > nrOfOne)
+}
+
+fun co2ScrubberRating() = { numberInformation:MutableMap<Int, NumberInformation>, binaryNumber:String, pos:Int ->
+    val nrOfOne = numberInformation[pos]!!.nrOfOne
+    val nrOfZero = numberInformation[pos]!!.nrOfZero
+    (binaryNumber[pos] == '1' && nrOfOne < nrOfZero)
+            ||
+            (binaryNumber[pos] == '0' && nrOfZero <= nrOfOne)
+}
+
+fun flipBinay(binay: String): String {
     var finalString = ""
     binay.forEach {
         if (it == '1') {
@@ -50,10 +98,10 @@ fun flipBinay(binay:String):String {
     return finalString
 }
 
-fun binaryToNumber(binary:String): Int {
+fun binaryToNumber(binary: String): Int {
     var finalNumber = 0
     for (i in binary.indices) {
-        val exponent = binary.length-i-1
+        val exponent = binary.length - i - 1
         val digit = binary[i].toString().toInt()
 
         finalNumber += digit * 2.0.pow(exponent.toDouble()).toInt()
@@ -62,19 +110,30 @@ fun binaryToNumber(binary:String): Int {
     return finalNumber
 }
 
-data class NumberInformation(var nrOfOne:Int=0, var nrOfZero:Int = 0) {
+data class NumberInformation(var nrOfOne: Int = 0, var nrOfZero: Int = 0) {
 
-    fun commonString():String {
-        return if (nrOfOne > nrOfZero)
+    fun mostCommonString(): String {
+        return if (nrOfOne >= nrOfZero)
             "1"
-        else if (nrOfZero > nrOfOne)
+        else
+            "0"
+    }
+
+    fun leastCommonString(): String {
+        return if (nrOfOne <= nrOfZero)
             "0"
         else
-            throw IllegalStateException("both equal")
+            "1"
     }
 
 }
 
 fun main() {
-    println(binaryDiagnostic(readInput("Day03", object {}::class.java)))
+    val binaryNumbers = readInput("Day03", object {}::class.java)
+    println("${binaryDiagnostic(binaryNumbers)}")
+
+    val oxygenGeneratorRating = lifeSupportingRate(binaryNumbers, oxygenGeneratorRating())
+    val co2ScrubberRating = lifeSupportingRate(binaryNumbers, co2ScrubberRating())
+
+    println(oxygenGeneratorRating * co2ScrubberRating)
 }
--- src/test/kotlin/day3/Day3Test.kt
+++ src/test/kotlin/day3/Day3Test.kt
@@ -14,4 +14,20 @@ class Day3Test {
         val binaryNumbers = readInput("binaryNumbers", Day3Test::class.java)
         Assertions.assertEquals(198, binaryDiagnostic(binaryNumbers))
     }
+
+    @Test
+    fun oxygenGenerator() {
+        val binaryNumbers = readInput("binaryNumbers", Day3Test::class.java)
+
+        val oxygenGenerator = lifeSupportingRate(binaryNumbers, oxygenGeneratorRating())
+        Assertions.assertEquals(23, oxygenGenerator)
+    }
+
+    @Test
+    fun co2Generator() {
+        val binaryNumbers = readInput("binaryNumbers", Day3Test::class.java)
+
+        val co2ScrubberGenerator = lifeSupportingRate(binaryNumbers, co2ScrubberRating())
+        Assertions.assertEquals(10, co2ScrubberGenerator)
+    }
 }
